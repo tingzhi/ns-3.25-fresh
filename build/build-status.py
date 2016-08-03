8c7053ac0cb965821f453c8cb0d51606ddfa4292
Tingzhi Li <vincentltz@localhost.localdomain>
2016-08-02 17:20:48 -0700

    ignore

--- build/build-status.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-# Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/ns3.25-scratch-simulator-debug', 'build/scratch/subdir/ns3.25-subdir-debug']
-
-# Scripts that are runnable.
-ns3_runnable_scripts = []
-
